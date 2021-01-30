f2ca2e3bfc97c7f35cbe90374838b9cc126f30cb
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2021-01-30 15:35:32 +0700

    new

--- productweb/models.py
+++ productweb/models.py
@@ -5,10 +5,11 @@ class Products(models.Model):
     price = models.FloatField(default=0)
     despation = models.CharField(max_length=500)
     year = models.IntegerField(default=0)
+    image = models.ImageField(upload_to= None, height_field= None, width_field= None, max_length= 1000)
     category = models.CharField(max_length=100)
 
     def __str__(self):
         s = "Tên sản phẩm:" + str(self.name) + ", Giá sản phẩm:" + str(self.price) + ", Mô tả:" \
-            + str(self.despation) + ", Năm sản xuất" + str(self.year) + ",Thể loại:" + str(self.category)
+            + str(self.despation) + ", Năm sản xuất" + str(self.year) + ", Ảnh:" + str(self.image) + ",Thể loại:" + str(self.category)
         return s
 
